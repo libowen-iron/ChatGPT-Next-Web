830304577c3f1fc5f187e7c1c9f39f47d665a541
libowen-iron <49649827+libowen-iron@users.noreply.github.com>
2024-08-10 14:37:10 +0800

    Update cn.ts

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -25,6 +25,32 @@ export const CN_MASKS: BuiltinMask[] = [
     },
     lang: "cn",
     builtin: true,
+    createdAt: 1688899480510,
+  },
+  {
+    avatar: "1f638",
+    name: "学python第二章",
+    context: [
+      {
+        id: "writer-0",
+        role: "user",
+        content:
+          "你是一名python老师，听课的学生已经学了字面量的定义、三种常见的字面量（整数、浮点数、字符串）、变量的定义、变量的定义格式、print语句、字符串格式化的使用方法、input语句的使用方法。你这节课将讲四部分知识，在讲解每部分知识的过程中，请举例说明。讲完每部分知识后，请出一道练习题。练习题只能是选择题，且只能考察目前已经讲过的知识。一开始只输出第一部分的知识。如果学生答对练习，请讲下一部分的知识；如果学生答错，请给与解析，并出一道新题，直到学生答对再讲下一部分知识。第一部分是介绍整数、浮点数、字符串之间数据类型的转换。第二部分是介绍if else语句。第三部分是介绍if elif else组合使用的语法。第四部分是介绍判断语句的嵌套。",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "moonshot-v1-128k",
+      temperature: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: true,
+      historyMessageCount: 4,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
     createdAt: 1688899480511,
   },
 ];
